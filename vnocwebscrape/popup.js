c9ed13d1c58642fa952b1ea3222c9269da2e17ec
GlobalVentures.com <maidabarrientos@gmail.com>
2018-05-21 14:40:04 +0800

    added popup modal

--- vnocwebscrape/popup.js
+++ vnocwebscrape/popup.js
@@ -1,160 +1,173 @@
 document.getElementById("startwebcrape").addEventListener('click', () => {
 	function modifyDOM() {
 		
-			if($('#selector').length==0){
-				$("body").append('<div id="selector">'+
-				'<div id="selector-top"></div>'+
-				'<div id="selector-left"></div>'+
-				'<div id="selector-right"></div>'+
-				'<div id="selector-bottom"></div>'+
-				'</div>');
-				
-				var $head = $("head"); 
-				var css = '<style>'+
-				
-				'#selector-top, #selector-bottom {'+
-					'background: blue;'+
-					'height:3px;'+
-					'position: fixed;'+
-					'transition:all 300ms ease;'+
-					'z-index:99999;'+
-				'}'+
-				'#selector-left, #selector-right {'+
-					'background: blue;'+
-					'width:3px;'+
-					'position: fixed;'+
-					'transition:all 300ms ease;'+
-					'z-index:99999;'+
-				'}'+
+		if($('#selector').length==0){
+			$("body").append('<div id="selector">'+
+			'<div id="selector-top"></div>'+
+			'<div id="selector-left"></div>'+
+			'<div id="selector-right"></div>'+
+			'<div id="selector-bottom"></div>'+
+			'</div>');
+			
+			var $head = $("head"); 
+			var css = '<style>'+
+			
+			'#selector-top, #selector-bottom {'+
+				'background: blue;'+
+				'height:3px;'+
+				'position: fixed;'+
+				'transition:all 300ms ease;'+
+				'z-index:99999;'+
+			'}'+
+			'#selector-left, #selector-right {'+
+				'background: blue;'+
+				'width:3px;'+
+				'position: fixed;'+
+				'transition:all 300ms ease;'+
+				'z-index:99999;'+
+			'}'+
 
-				'.n{'+
-				 '-webkit-transform: scale(3) translateX(100px)'+
-				'}'+
+			'.n{'+
+			 '-webkit-transform: scale(3) translateX(100px)'+
+			'}'+
 
-				'.selectedelement {'+
-					'border-width: 2px !important;'+
-					'border-color: red !important;'+
-					'border-style: solid !important;'+
-				'}';
-				
-				'</style>';
-				$head.append(css);
-				
-				$("body").prepend('<div id="tbl-container" class="stdform" style="margin-left:10px">'+
-				'<hr>'+
-				'<button class="btn btn-primary btn-proceed btn-tbl" style="display:none;background: #333;margin-bottom:10px">Proceed to Research</button>'+
-				'<button style="display:none;background: #333;margin-bottom:10px" class="btn-tbl btn btn-danger  btn-remove">Delete Selected</button>'+
-				'<form id="frmprocess" method="POST" action="https://manage.vnoc.com/research/tool" target="_blank">'+
-				'</form>'+
-				'<table border=1 id="result" class="stdtable" style="background: #fff;"></table>'+
-				'</div>');
-				
-				
-				jQuery('.btn-remove').click(function(){
-					jQuery('.chck_row:checked').parents('tr').remove();
-					jQuery('.btn-remove').hide();
-					showFields();
-				});
-				
-				jQuery('.btn-proceed').click(function(){
-					var data = [];
-					var x = 0;
-					jQuery('#frmprocess').html('');
-					jQuery('#result tr').not('#fields').each(function(){
-						var col = {name:'',email:'',address:'',phone:'',social:'',domains:''};
-						var hasValue = false;
+			'.selectedelement {'+
+				'border-width: 2px !important;'+
+				'border-color: red !important;'+
+				'border-style: solid !important;'+
+			'}';
+			
+			'</style>';
+			$head.append(css);
+			
+			var tbl = ('<div id="tbl-container" class="stdform" style="margin-left:10px">'+
+			'<button class="btn btn-primary btn-proceed btn-tbl" style="display:none;color:#ffff;background: #333;margin-bottom:10px">Proceed to Research</button>'+
+			'<button style="display:none;background: #333;margin-bottom:10px;color:#ffff;" class="btn-tbl btn btn-danger  btn-remove">Delete Selected</button>'+
+			'<form id="frmprocess" method="POST" action="https://manage.vnoc.com/research/tool" target="_blank">'+
+			'</form>'+
+			'<table border=1 id="result" class="stdtable" style="background: #fff;"></table>'+
+			'</div>');
+			
+			$(document).on('click','#tbl-container input[type=text]',function(){ this.select(); });
+			
+			if(document.getElementById('notifyvnoc')==undefined){
+				var css = '<style>.cd-popup{z-index:99999;position:fixed;left:0;top:0;height:100%;width:100%;background-color:rgba(94,110,141,0.9);opacity:0;visibility:hidden;-webkit-transition:opacity .3s 0s,visibility 0 .3s;-moz-transition:opacity .3s 0s,visibility 0 .3s;transition:opacity .3s 0s,visibility 0 .3s}.cd-popup.is-visible{opacity:1;visibility:visible;-webkit-transition:opacity .3s 0s,visibility 0 0;-moz-transition:opacity .3s 0s,visibility 0 0;transition:opacity .3s 0s,visibility 0 0}.cd-popup-container{overflow:auto;height: 70%;position:relative;margin:4em auto;background:#FFF;border-radius:.25em .25em .4em .4em;text-align:center;box-shadow:0 0 20px rgba(0,0,0,0.2);-webkit-transform:translateY(-40px);-moz-transform:translateY(-40px);-ms-transform:translateY(-40px);-o-transform:translateY(-40px);transform:translateY(-40px);-webkit-backface-visibility:hidden;-webkit-transition-property:-webkit-transform;-moz-transition-property:-moz-transform;transition-property:transform;-webkit-transition-duration:.3s;-moz-transition-duration:.3s;transition-duration:.3s}.cd-popup-container p{padding:3em 1em}.cd-popup-container .cd-buttons:after{content:"";display:table;clear:both}.cd-popup-container .cd-buttons li{float:left;width:50%}.cd-popup-container .cd-buttons a{display:block;height:60px;line-height:60px;text-transform:uppercase;color:#FFF;-webkit-transition:background-color .2s;-moz-transition:background-color .2s;transition:background-color .2s}.cd-popup-container .cd-buttons li:first-child a{background:#fc7169;border-radius:0 0 0 .25em}.no-touch .cd-popup-container .cd-buttons li:first-child a:hover{background-color:#fc8982}.cd-popup-container .cd-buttons li:last-child a{background:#b6bece;border-radius:0 0 .25em 0}.no-touch .cd-popup-container .cd-buttons li:last-child a:hover{background-color:#c5ccd8}.cd-popup-container .cd-popup-close{position:absolute;top:8px;right:8px;width:30px;height:30px}.cd-popup-container .cd-popup-close::before,.cd-popup-container .cd-popup-close::after{content:"";position:absolute;top:12px;width:14px;height:3px;background-color:#8f9cb5}.cd-popup-container .cd-popup-close::before{-webkit-transform:rotate(45deg);-moz-transform:rotate(45deg);-ms-transform:rotate(45deg);-o-transform:rotate(45deg);transform:rotate(45deg);left:8px}.cd-popup-container .cd-popup-close::after{-webkit-transform:rotate(-45deg);-moz-transform:rotate(-45deg);-ms-transform:rotate(-45deg);-o-transform:rotate(-45deg);transform:rotate(-45deg);right:8px}.is-visible .cd-popup-container{-webkit-transform:translateY(0);-moz-transform:translateY(0);-ms-transform:translateY(0);-o-transform:translateY(0);transform:translateY(0)}</style>';
+				var html = '<div id="notifyvnoc" class="cd-popup" role="alert">	<div  class="cd-popup-container"><div id="notifyvnocmsg">'+tbl+'</div><ul class="cd-buttons"></ul><a href="#0" class="cd-popup-close img-replace">&nbsp;</a></div></div>';
+				jQuery('body').append(css+html);
+			}
+			
+			jQuery('.cd-popup').off('click').on('click', function(event){
+				if( jQuery(event.target).is('.cd-popup-close') || jQuery(event.target).is('.cd-popup') ) {
+					event.preventDefault();
+					jQuery(this).removeClass('is-visible');
+				}
+			});
+			
+			jQuery('.btn-remove').click(function(){
+				jQuery('.chck_row:checked').parents('tr').remove();
+				jQuery('.btn-remove').hide();
+				showFields();
+			});
+			
+			jQuery('.btn-proceed').click(function(){
+				var data = [];
+				var x = 0;
+				jQuery('#frmprocess').html('');
+				jQuery('#result tr').not('#fields').each(function(){
+					var col = {name:'',email:'',address:'',phone:'',social:'',domains:''};
+					var hasValue = false;
 
-						jQuery(this).find('td').not(':first').each(function(){
-							var field = jQuery(this).attr('data-field');
-							if(field!='' && field!=undefined){
-								col[field] += jQuery(this).html()+" ";
-								hasValue = true;
-							}
-						});
-						
-						if(hasValue){
-							data.push(col);
-							jQuery('#frmprocess').append('<input name="people['+x+'][name]" type="hidden" value="'+col.name.replace("'",'').replace('"','')+'" />');
-							jQuery('#frmprocess').append('<input name="people['+x+'][email]" type="hidden" value="'+col.email.replace("'",'').replace('"','')+'" />');
-							jQuery('#frmprocess').append('<input name="people['+x+'][address]" type="hidden" value="'+col.address.replace("'",'').replace('"','')+'" />');
-							jQuery('#frmprocess').append('<input name="people['+x+'][phone]" type="hidden" value="'+col.phone.replace("'",'').replace('"','')+'" />');
-							jQuery('#frmprocess').append('<input name="people['+x+'][social]" type="hidden" value="'+col.social.replace("'",'').replace('"','')+'" />');
-							jQuery('#frmprocess').append('<input name="people['+x+'][domains]" type="hidden" value="'+col.domains.replace("'",'').replace('"','')+'" />');
-							jQuery('#frmprocess').append('<input name="people['+x+'][twitter]" type="hidden" value="" />');
-							x++;
+					jQuery(this).find('td').not(':first').each(function(){
+						var field = jQuery(this).attr('data-field');
+						if(field!='' && field!=undefined){
+							col[field] += jQuery(this).find('input').val()+" ";
+							hasValue = true;
 						}
 					});
 					
-					if(jQuery('#frmprocess').html()==''){
-						alert('Please select column for research');
-						return false;
-					}else{
-						jQuery('#frmprocess').submit();
+					if(hasValue){
+						data.push(col);
+						jQuery('#frmprocess').append('<input name="people['+x+'][name]" type="hidden" value="'+col.name.replace("'",'').replace('"','')+'" />');
+						jQuery('#frmprocess').append('<input name="people['+x+'][email]" type="hidden" value="'+col.email.replace("'",'').replace('"','')+'" />');
+						jQuery('#frmprocess').append('<input name="people['+x+'][address]" type="hidden" value="'+col.address.replace("'",'').replace('"','')+'" />');
+						jQuery('#frmprocess').append('<input name="people['+x+'][phone]" type="hidden" value="'+col.phone.replace("'",'').replace('"','')+'" />');
+						jQuery('#frmprocess').append('<input name="people['+x+'][social]" type="hidden" value="'+col.social.replace("'",'').replace('"','')+'" />');
+						jQuery('#frmprocess').append('<input name="people['+x+'][domains]" type="hidden" value="'+col.domains.replace("'",'').replace('"','')+'" />');
+						jQuery('#frmprocess').append('<input name="people['+x+'][twitter]" type="hidden" value="" />');
+						x++;
 					}
 				});
-			}
-			
-			var elements = {
-				top: $("body").find('#selector-top'),
-				left: $("body").find('#selector-left'),
-				right: $("body").find('#selector-right'),
-				bottom: $("body").find('#selector-bottom')
-			};
-
-			$("body").find('a').each(function(){
-				$(this).replaceWith($(this).attr('href','javascript:;').removeAttr('target').clone());
+				
+				if(jQuery('#frmprocess').html()==''){
+					alert('Please select column for research');
+					return false;
+				}else{
+					jQuery('#frmprocess').submit();
+				}
 			});
-			
-			$("body").mousemove(function(event) {
-				if(event.target.id.indexOf('selector') !== -1 || event.target.tagName === 'BODY' || event.target.tagName === 'HTML') return;
-				var $target = $(event.target);
-				
-				targetElement = $target;
-					targetOffset = $target[0].getBoundingClientRect(),
-					targetHeight = targetOffset.height,
-					targetWidth  = targetOffset.width;
-				
-				
-				if($(targetElement).parents('#tbl-container').length>0) return;
-				
-				var elementType = $($target).get(0).tagName;
-				
-				elements.top.css({
-					left:  (targetOffset.left - 4),
-					top:   (targetOffset.top - 4),
-					width: (targetWidth + 5)
-				});
-				elements.bottom.css({
-					top:   (targetOffset.top + targetHeight + 1),
-					left:  (targetOffset.left  - 3),
-					width: (targetWidth + 4)
-				});
-				elements.left.css({
-					left:   (targetOffset.left  - 5),
-					top:    (targetOffset.top  - 4),
-					height: (targetHeight + 8)
-				});
-				elements.right.css({
-					left:   (targetOffset.left + targetWidth + 1),
-					top:    (targetOffset.top  - 4),
-					height: (targetHeight + 8)
-				});
-				
-			});
-			
+		}
 		
+		$('#selector').show();
+		$('.selectedelementhide').removeClass('selectedelementhide').addClass('selectedelement');
+		
+		var elements = {
+			top: $("body").find('#selector-top'),
+			left: $("body").find('#selector-left'),
+			right: $("body").find('#selector-right'),
+			bottom: $("body").find('#selector-bottom')
+		};
+
+		$("body").find('a').each(function(){
+			$(this).replaceWith($(this).attr('href','javascript:;').removeAttr('target').clone());
+		});
+		
+		$("body").mousemove(function(event) {
+			if(event.target.id.indexOf('selector') !== -1 || event.target.tagName === 'BODY' || event.target.tagName === 'HTML') return;
+			var $target = $(event.target);
+			
+			targetElement = $target;
+				targetOffset = $target[0].getBoundingClientRect(),
+				targetHeight = targetOffset.height,
+				targetWidth  = targetOffset.width;
+			
+			
+			if($(targetElement).parents('#tbl-container').length>0) return;
+			
+			var elementType = $($target).get(0).tagName;
+			
+			elements.top.css({
+				left:  (targetOffset.left - 4),
+				top:   (targetOffset.top - 4),
+				width: (targetWidth + 5)
+			});
+			elements.bottom.css({
+				top:   (targetOffset.top + targetHeight + 1),
+				left:  (targetOffset.left  - 3),
+				width: (targetWidth + 4)
+			});
+			elements.left.css({
+				left:   (targetOffset.left  - 5),
+				top:    (targetOffset.top  - 4),
+				height: (targetHeight + 8)
+			});
+			elements.right.css({
+				left:   (targetOffset.left + targetWidth + 1),
+				top:    (targetOffset.top  - 4),
+				height: (targetHeight + 8)
+			});			
+		});
 	
 		$("body").on("click",function() {
 			clickEelement = targetElement;
 			if($(clickEelement).parents('#tbl-container').length>0) return;
 			process2();
-			return true;
-		});
-		
-		$("body").contextmenu(function() {
-			clickEelement = targetElement;
-			if($(clickEelement).parents('#tbl-container').length>0) return;
-			process2();
+
+			jQuery('.cd-popup').addClass('is-visible');
+			$("body").off('click');
+			$("body").off('mousemove');
+			$('#selector').hide();
+			$('.selectedelement').removeClass('selectedelement').addClass('selectedelementhide');
+			
 			return true;
 		});
 	
@@ -361,7 +374,7 @@ document.getElementById("startwebcrape").addEventListener('click', () => {
 				row = $('<tr />');
 				row.append($('<td />').html('<input class="chck_row" type="checkbox" value="" />'));
 				for (j = 0; j < data[i].length; j++) {
-					row.append($('<td />').html(data[i][j]));
+					row.append($('<td />').html("<input type='text' value='"+data[i][j]+"' />"));
 				}
 				rows.push(row);
 			}
@@ -454,7 +467,7 @@ document.getElementById("startwebcrape").addEventListener('click', () => {
 				if($(this).is(':checked')){
 					$('.btn-remove').show();
 				}else{
-					$('.btn-remove').hide();
+					if($('.chck_row:checked').length==0) $('.btn-remove').hide();
 				}
 			});
 		}
@@ -472,7 +485,7 @@ document.getElementById("startwebcrape").addEventListener('click', () => {
 			var msgid = msgid==undefined?'loadervnoc':msgid;
 			jQuery('body').find('#'+msgid).remove();
 		}
-	
+		
         return document.body.innerHTML;
     }
 
